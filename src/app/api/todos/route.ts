d4fb24f568173c0ef584b73e92f578c426f1d49b
Oluwademilade Ala <demiladeala@gmail.com>
2023-10-31 18:09:24 +0100

    comment out todos route

--- src/app/api/todos/route.ts
+++ src/app/api/todos/route.ts
@@ -1,42 +1,42 @@
-// import { NextResponse, NextRequest } from "next/server";
-// import dbClient from "@/app/utils/dbClient";
-// import server from "@/app/utils/supabaseConnection";
+import { NextResponse, NextRequest } from "next/server";
+import dbClient from "@/app/utils/dbClient";
+import server from "@/app/utils/supabaseConnection";
 
 
-// type RequestParams = {
-//     title:string, content:string, isDelete:boolean, 
-// }
+type RequestParams = {
+    title:string, content:string, isDelete:boolean, 
+}
 
-// export async function POST(req: NextRequest) {
-//     const body = await req.json() as RequestParams
+export async function POST(req: NextRequest) {
+    const body = await req.json() as RequestParams
 
 
-//     //from the session, you will need to get the logged in user 
-//     const result = (await server.auth.getSession()).data.session?.user;
-//     console.log(result);
+    //from the session, you will need to get the logged in user 
+    const result = (await server.auth.getSession()).data.session?.user;
+    console.log(result);
 
-//     //find if that user exist on the user table and get the Id 
+    //find if that user exist on the user table and get the Id 
 
-//         const user = await dbClient.user.findFirst({
-//             where: {
-//                 email: result?.email
-//             }
-//         })
-//         console.log(user);
+        const user = await dbClient.user.findFirst({
+            where: {
+                email: result?.email
+            }
+        })
+        console.log(user);
 
-//         const userId = user?.id
+        const userId = user?.id
 
-// // Insert the userId into the Todos table as the user creating the todo.
-//     if(userId !== undefined) {
-//         const todoResult = await dbClient.todos.create({
-//             data: {
-//                 content: body.content,
-//                 isDelete: false,
-//                 userId: userId,
-//             }
-//         })
-//         console.log(todoResult);
-//         return NextResponse.json(todoResult);
-//     }
+// Insert the userId into the Todos table as the user creating the todo.
+    // if(userId !== undefined) {
+    //     const todoResult = await dbClient.todos.create({
+    //         data: {
+    //             content: body.content,
+    //             isDelete: false,
+    //             userId: userId,
+    //         }
+    //     })
+    //     console.log(todoResult);
+    //     return NextResponse.json(todoResult);
+    // }
 
-// }
+}
